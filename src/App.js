9c2e89a257e3ca299d47184928a3f4f5fd337d6f
samndambuki <sam.ndambuki@outlook.com>
2024-10-12 17:53:21 +0300

    refactored app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import './App.css';
 import Header from './Header';
 import Home from './Home'
 import Checkout from './Checkout';
-import { BrowserRouter as Router,Route,Switch,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/checkout">Checkout</Link>
-      </nav>
         <Switch>
           <Route path="/checkout">
           <Header></Header>
