662c14ff635cee46930065166053d3a05b7e136d
samndambuki <sam.ndambuki@outlook.com>
2024-11-25 08:35:30 +0300

    worked on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,8 @@
 /** @format */
-
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal.js";
 
 function Checkout() {
   const product = {
@@ -17,7 +17,7 @@ function Checkout() {
         <img
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
-          className="checkout__add"
+          className="checkout__ad"
         />
         <div>
           <h2 className="checkout__title">Your shopping basket</h2>
@@ -29,9 +29,10 @@ function Checkout() {
           />
         </div>
       </div>
-      <div className="checkout__right"></div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
     </div>
   );
 }
-
 export default Checkout;
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,37 @@
 /** @format */
+
 import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+
 function Subtotal() {
-  return <div></div>;
+  // const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Subtotal ({basket.length} items): <strong>${value}</strong> */}
+              <p>
+                Subtotal (0) items :<strong>$0</strong>
+              </p>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        // value={getBasketTotal(basket)}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;
